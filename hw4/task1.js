4fd0071db9fc90f7c280fd61f1075088c71ece55
Oleg Savchuk <ol.sav4yk@gmail.com>
2022-02-16 23:02:06 +0200

    init commit

---
+++ hw4/task1.js
@@ -0,0 +1,36 @@
+/*Напишите свою имплементацию функции `bind`
+
+Принимает такие параметры:
+
+- Первый параметр — функция которую мы хотим привязать
+- Второй параметр — ссылка на this
+- Третий параметр до ...n — параметры для функции которую мы хотим привязать
+
+Пример вызова:
+
+```javascript
+const obj = {
+getName: function(message) {
+return `${message}: ${this.name}`;
+}
+};
+
+const getName = obj.getName;
+
+const f = bind(getName, {name: 'Pitter'}, 'My name');
+console.log(f()); // My name: Pitter
+```*/
+function bind(f, context, ...args) {
+    return () => f.call(context, ...args)
+  }
+  
+  const obj = {
+    getName: function(message) {
+    return `${message}: ${this.name}`;
+    }
+    };
+    
+    const getName = obj.getName;
+    
+    const f = bind(getName, {name: 'Pitter'}, 'My name');
+    console.log(f()); // My name: Pitter
